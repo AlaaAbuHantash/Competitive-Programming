2727643e80229fb663080da44260499a7b0e6c27
Alaa Abu-Hantash <alaa.m.abuhantash@gmail.com>
2018-04-26 12:57:43 +0300

    Add files via upload

---
+++ Live Archive/2189 - Mobile Casanova.cpp
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <set>
+#include <utility> 
+#include <stack>
+#include <cstring> 
+#include <math.h>
+#include<cstdio>
+#include<deque>
+#include<sstream>
+using namespace std;
+int dx[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+int dy[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+#define mp make_pair
+int main() {
+
+	//freopen("test.txt", "rt", stdin);
+	//freopen("output.txt", "w", stdout);
+	int n,t=1; 
+	while (scanf("%d", &n) > 0){
+		if (!n)
+			break;
+		vector<string>v;
+		printf("Case %d:\n", t++);
+		for (int i = 0; i < n; i++){
+			string s;
+			cin >> s;
+			v.push_back(s);
+		}
+		int beg = 0,c=0;
+		for (int i = 1; i < n; i++){
+			if (atoi(v[i - 1].c_str()) + 1 == atoi(v[i].c_str())){
+				c++;
+			}
+			else{
+				int j;
+				for (j = 0; j < v[beg].length(); j++){
+					if (v[beg][j] == v[i - 1][j])
+						printf("%c", v[beg][j]);
+					else
+						break;
+				}
+				for (int k = j; k < v[beg].length(); k++)
+						printf("%c", v[beg][k]);
+				if (c){
+					printf("-");
+					for (int k = j; k < v[beg].length(); k++)
+						printf("%c", v[i-1][k]);
+				}
+				c = 0;
+				beg = i;
+				puts("");
+			}
+
+		}
+		int j=0;
+		for (j = 0; j < v[beg].length(); j++){
+			if (v[beg][j] == v[beg+c][j])
+				printf("%c", v[beg][j]);
+			else
+				break;
+		}
+		for (int k = j; k < v[beg].length(); k++)
+			printf("%c", v[beg][k]);
+		if (c){
+			printf("-");
+			for (int k = j; k < v[beg].length(); k++)
+				printf("%c", v[beg+c][k]);
+		}
+		puts("");
+		c = 0;
+		puts("");
+	}
+	return 0;
+}
+
---
+++ Live Archive/2538 - Tanning Salon.cpp
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <set>
+#include <utility> 
+#include <stack>
+#include <cstring> 
+#include <math.h>
+#include<cstdio>
+#include<deque>
+#include<sstream>
+using namespace std;
+int dx[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+int dy[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+const int M = 1e9 + 7;
+int a[33];
+int main() {
+
+	//freopen("test.txt", "rt", stdin);
+	//freopen("output.txt", "w", stdout);
+
+	int n; 
+	string s;
+	while (cin >> n){
+		if (!n)
+			break;
+		cin >> s;
+		memset(a, 0, sizeof(a));
+		int len = s.length(),c=0,res=0,all=0;
+		for (int i = 0; i < len; i++){
+			if (a[s[i] - 'A'] == 0 && c < n){
+				c++, a[s[i] - 'A']++, res++;
+				all++;
+			}
+			else if (a[s[i] - 'A'] == 0)
+				a[s[i] - 'A'] = 2,all++; 
+			else if (a[s[i] - 'A'] == 1)
+				c--;
+
+		}
+		//cout << res << endl;
+		if (res == all)
+			puts("All customers tanned successfully.");
+		else
+			printf("%d customer(s) walked away.\n", all - res);
+	}
+
+	return 0;
+}
+
