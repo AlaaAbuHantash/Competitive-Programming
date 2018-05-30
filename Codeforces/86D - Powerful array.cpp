6287e052ab365469bb7d6860a4106008dbd799f8
Alaa Abu-Hantash <alaa.m.abuhantash@gmail.com>
2018-05-30 11:30:33 +0300

    Update 86D - Powerful array.cpp

--- Codeforces/86D - Powerful array.cpp
+++ Codeforces/86D - Powerful array.cpp
@@ -1,89 +1,82 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <queue>
-#include <map>
-#include <set>
-#include <utility>
-#include <stack>
-#include <cstring>
-#include <math.h>
-#include<cstdio>
-#include<deque>
-#include<sstream>
-#define mp make_pair
-#define eps 1e-6
+#include <bits/stdc++.h>
 using namespace std;
-int dx[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
-int dy[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
-int a[200004];
-int cnt[1000002];
-long long qq[200001];
-int n, m, x, y, sq;
-struct st {
-	int x, y, ind;
-} v[200001];
-bool cmp(st a, st b) {
-	if (a.x / sq < b.x / sq)
-		return 1;
-	if (b.x / sq < a.x / sq)
-		return 0;
-	return a.y < b.y;
+#define mp make_pair
+int a[200005];
+int f[1000005];
+long long ans[200005];
+pair<pair<int, int>, int> v[200005];
+int blk;
+inline bool comp(const pair<pair<int, int>, int> &a,
+		const pair<pair<int, int>, int> &b) {
+	int la = a.first.first / blk;
+	int lb = b.first.first / blk;
+	if (la != lb)
+		return la < lb;
+	int ra = a.first.second / blk;
+	int rb = b.first.second / blk;
+	return ra < rb;
 }
-int main() {
-	//freopen("b.txt", "rt", stdin);
+int main(int argc, char **argv) {
+	//freopen("a.txt", "r", stdin);
+	int n, m;
 	scanf("%d%d", &n, &m);
-	for (int i = 1; i <= n; i++)
-		scanf("%d", &a[i]);
-	sq = sqrt(n);
-	for (int i = 0; i < m; i++) {
-		scanf("%d%d", &v[i].x, &v[i].y);
-		v[i].ind = i;
+	blk = ceil(sqrt(n));
+	for (int i = 0; i < n; i++) {
+		scanf("%lld", &a[i]);
 	}
-	sort(v, v + m, cmp);
-	int xx = 0, yy = 0;
-	long long res = 0;
-	for (int i = 0; i < m; i++) {
-		x = v[i].x, y = v[i].y;
-		while (xx < x) {
-			res -= (long long) cnt[a[xx]] * (long long) cnt[a[xx]]
-					* (long long) a[xx];
-			cnt[a[xx]]--;
-			res += (long long) (cnt[a[xx]] * (long long) cnt[a[xx]]
-					* (long long) a[xx]);
-			xx++;
-		}
-		while (xx > x) {
-			xx--;
-			res -= (long long) cnt[a[xx]] * (long long) cnt[a[xx]]
-					* (long long) a[xx];
-			cnt[a[xx]]++;
-			res += (long long) (cnt[a[xx]] * (long long) cnt[a[xx]]
-					* (long long) a[xx]);
-		}
-		while (yy < y) {
-			yy++;
-			res -= (long long) cnt[a[yy]] * (long long) cnt[a[yy]]
-					* (long long) a[yy];
-			cnt[a[yy]]++;
-			res += (long long) cnt[a[yy]] * (long long) cnt[a[yy]]
-					* (long long) a[yy];
-		}
-		while (yy > y) {
-			res -= (long long) cnt[a[yy]] * (long long) cnt[a[yy]]
-					* (long long) a[yy];
-			cnt[a[yy]]--;
-			res += (long long) cnt[a[yy]] * (long long) cnt[a[yy]]
-					* (long long) a[yy];
-			yy--;
-		}
-		qq[v[i].ind] = res;
-		//cout << res << endl;
+	int x, y;
+	for (int k = 0; k < m; k++) {
+		scanf("%d%d", &x, &y);
+		x--, y--;
+		v[k].first.first = x;
+		v[k].first.second = y;
+		v[k].second = k;
 	}
-	for (int i = 0; i < m; i++)
-		printf("%I64d\n", qq[i]);
+	sort(v, v + m, comp);
+	int st = 0, ed = -1;
+	long long sum = 0;
+
+	for (int k = 0; k < m; k++) {
+		x = v[k].first.first;
+		y = v[k].first.second;
+		int ind = v[k].second;
+		if (x > st) {
+			for (int i = st; i < x; i++) {
+				sum -= (long long) f[a[i]] * f[a[i]] * a[i];
+				f[a[i]]--;
+				sum += (long long) f[a[i]] * f[a[i]] * a[i];
+			}
+
+		} else if (x < st) {
+			for (int i = st - 1; i >= x; i--) {
+				sum -= (long long) f[a[i]] * f[a[i]] * a[i];
+				f[a[i]]++;
+				sum += (long long) f[a[i]] * f[a[i]] * a[i];
+			}
+		}
+
+		if (y > ed) {
+			for (int i = ed + 1; i <= y; i++) {
+				sum -= (long long) f[a[i]] * f[a[i]] * a[i];
+				f[a[i]]++;
+				sum += (long long) f[a[i]] * f[a[i]] * a[i];
+			}
+
+		} else if (y < ed) {
+			for (int i = ed; i > y; i--) {
+				sum -= (long long) f[a[i]] * f[a[i]] * a[i];
+				f[a[i]]--;
+				sum += (long long) f[a[i]] * f[a[i]] * a[i];
+			}
+
+		}
+		st = x, ed = y;
+		ans[ind] = sum;
+	}
+
+	for (int i = 0; i < m; i++) {
+		printf("%lld\n", ans[i]);
+	}
 
 	return 0;
 }
